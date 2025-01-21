533ac3ffe25540995c3cac5e43da86fe7cdcd248
ananthu <heyananthulal@gmail.com>
2025-01-21 13:22:26 +0530

    second commit 4

--- client/src/Components/User/UserPages/UserHome/BottomNav.jsx
+++ client/src/Components/User/UserPages/UserHome/BottomNav.jsx
@@ -7,20 +7,17 @@ import { useNavigate } from 'react-router-dom';
 function BottomNav() {
     const navigate = useNavigate()
     return (
-        <div>
-            <div className="bg-white fixed bottom-0 left-0 p-4 h-20 w-full shadow-lg flex justify-around items-center sm:hidden md:hidden">
-                <div className='flex gap-11 text-black ml-2 mt-4'>
-                    <FaHome size={40} className='cursor-pointer' />
-                    <IoChatboxOutline size={40} className='cursor-pointer' />
-
-                    <p className='text-2xl font-sans mt-4'>Sell</p>
-                    <TiDocumentText size={40} className='cursor-pointer' />
-                    <VscAccount size={40} className='cursor-pointer' />
-                </div>
+        <div className='bg-white  left-0 top-[52rem] bottom-0 p-2 h-32 w-full fixed sm:hidden md:hidden'>
+            <div className='flex gap-11 text-black ml-2 mt-4'>
+                <FaHome size={40} className='cursor-pointer' />
+                <IoChatboxOutline size={40} className='cursor-pointer' />
+                <p className='rounded-full font-bold fixed left-[10rem] top-[48.5rem] w-24 h-24 text-6xl text-center bg-white border-8 border-l-green-500 border-t-red-600 border-r-yellow-300 border-b-blue-600 p-1 cursor-pointer'
+                    onClick={() => { navigate('/sellfirst') }}
+                >+</p>
+                <p className='text-2xl font-sans mt-4'>Sell</p>
+                <TiDocumentText size={40} className='cursor-pointer' />
+                <VscAccount size={40} className='cursor-pointer' />
             </div>
-            <p className='rounded-full font-bold fixed left-[10.5rem] top-[49.5rem] bottom-[8rem] w-[6rem] h-[6rem] text-black text-6xl text-center bg-white border-8 border-l-green-500 border-t-red-600 border-r-yellow-300 border-b-blue-600 p-1 cursor-pointer'
-                        onClick={() => { navigate('/sellfirst') }}
-                    >+</p>
         </div>
     )
 }
